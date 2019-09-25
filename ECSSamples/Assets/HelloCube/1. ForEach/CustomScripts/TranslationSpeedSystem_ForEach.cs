590d17a214290a0dc67836fbd0c23547aae49f53
MTrecozzi <47111522+MTrecozzi@users.noreply.github.com>
2019-09-25 16:26:10 -0400

    update

---
+++ ECSSamples/Assets/HelloCube/1. ForEach/CustomScripts/TranslationSpeedSystem_ForEach.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+// This system updates all entities in the scene with both a RotationSpeed_ForEach and Rotation component.
+
+// ReSharper disable once InconsistentNaming
+public class TranslationSpeedSystem_ForEach : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        // Entities.ForEach processes each set of ComponentData on the main thread. This is not the recommended
+        // method for best performance. However, we start with it here to demonstrate the clearer separation
+        // between ComponentSystem Update (logic) and ComponentData (data).
+        // There is no update logic on the individual ComponentData.
+        Entities.ForEach((ref TranslationSpeed_ForEach translation, ref Rotation rotation) =>
+        {
+            var deltaTime = Time.deltaTime;
+            translation.pos.Value += translation.pos.Value;
+            
+        });
+    }
+}
---
+++ ECSSamples/Assets/HelloCube/1. ForEach/CustomScripts/TranslationSpeed_ForEach.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Transforms;
+
+// Serializable attribute is for editor support.
+// ReSharper disable once InconsistentNaming
+[Serializable]
+public struct TranslationSpeed_ForEach : IComponentData
+{
+    public float speed;
+    public LocalToWorld pos;
+    
+}
